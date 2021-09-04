d6069267e27e5232101895a9d5a3a521d3ded7c7
wetarteam <78031947+wetarteam@users.noreply.github.com>
2021-09-05 02:43:30 +0530

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -282,7 +282,9 @@ Expires October 25, 2021{"\n\n"}
 					background="#fedb59"
 				>
 					<Override slot="StackItemContent" flex-direction="column" align-items="center" />
-					<Components.QuarklycommunityKitCounter font="--headline1" startProp="0" endProp="150" />
+					<Text font="--headline1">
+						150
+					</Text>
 					<Text
 						as="h3"
 						font="--headline3"
@@ -306,7 +308,9 @@ Expires October 25, 2021{"\n\n"}
 					sm-background="#28eba5"
 				>
 					<Override slot="StackItemContent" flex-direction="column" align-items="center" />
-					<Components.QuarklycommunityKitCounter font="--headline1" startProp="0" endProp="150" />
+					<Text font="--headline1">
+						5
+					</Text>
 					<Text
 						as="h3"
 						font="--headline3"
@@ -328,7 +332,9 @@ Expires October 25, 2021{"\n\n"}
 					sm-background="#7dfef0"
 				>
 					<Override slot="StackItemContent" flex-direction="column" align-items="center" />
-					<Components.QuarklycommunityKitCounter font="--headline1" startProp="0" endProp="8" />
+					<Text font="--headline1">
+						8
+					</Text>
 					<Text
 						as="h3"
 						font="--headline3"
@@ -456,7 +462,7 @@ Expires October 25, 2021{"\n\n"}
 			border-style="solid"
 		>
 			<Override slot="SectionContent" transition="all 3s ease-in 0s" />
-			<Components.QuarklycommunityKitLottie path="https://assets4.lottiefiles.com/packages/lf20_27cgfczo.json" width="700px" height="250px" />
+			<Components.QuarklycommunityKitLottie path="https://assets4.lottiefiles.com/packages/lf20_27cgfczo.json" />
 			<Text
 				font="normal 900 72px/1.2 --fontFamily-googleMontserrat"
 				text-align="center"
@@ -820,9 +826,6 @@ Expires October 25, 2021{"\n\n"}
 			<style place={"endOfHead"} rawKey={"60d350285179a1001e683fe6"}>
 				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
 			</style>
-			<script async={false} src={""} place={"endOfBody"} rawKey={"60ded04bedd6eb6b49543f2c"}>
-				{"var MessageBirdChatWidgetSettings = {     widgetId: '38492b3e-cde1-4b95-9226-d2b609b1a85e',     initializeOnLoad: true,   };  !function(){\"use strict\";if(Boolean(document.getElementById(\"live-chat-widget-script\")))console.error(\"MessageBirdChatWidget: Snippet loaded twice on page\");else{var e,t;window.MessageBirdChatWidget={},window.MessageBirdChatWidget.queue=[];for(var i=[\"init\",\"setConfig\",\"toggleChat\",\"identify\",\"hide\",\"on\",\"shutdown\"],n=function(){var e=i[d];window.MessageBirdChatWidget[e]=function(){for(var t=arguments.length,i=new Array(t),n=0;n<t;n++)i[n]=arguments[n];window.MessageBirdChatWidget.queue.push([[e,i]])}},d=0;d<i.length;d++)n();var a=(null===(e=window)||void 0===e||null===(t=e.MessageBirdChatWidgetSettings)||void 0===t?void 0:t.widgetId)||\"\",o=function(){var e,t=document.createElement(\"script\");t.type=\"text/javascript\",t.src=\"https://livechat.messagebird.com/bootstrap.js?widgetId=\".concat(a),t.async=!0,t.id=\"live-chat-widget-script\";var i=document.getElementsByTagName(\"script\")[0];null==i||null===(e=i.parentNode)||void 0===e||e.insertBefore(t,i)};\"complete\"===document.readyState?o():window.attachEvent?window.attachEvent(\"onload\",o):window.addEventListener(\"load\",o,!1)}}();"}
-			</script>
 		</RawHtml>
 	</Theme>;
 });
