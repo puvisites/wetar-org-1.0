b4a0d13bb69b205557f96bec051926717f354b75
wetarteam <78031947+wetarteam@users.noreply.github.com>
2021-09-19 22:24:18 +0530

    Update templates.js

--- src/pages/templates.js
+++ src/pages/templates.js
@@ -190,28 +190,7 @@ export default (() => {
 		</Section>
 		<Components.Footer3 />
 		<Components.FbChat />
-		<Link
-			font={"--capture"}
-			font-size={"10px"}
-			position={"fixed"}
-			bottom={"12px"}
-			right={"12px"}
-			z-index={"4"}
-			border-radius={"4px"}
-			padding={"5px 12px 4px"}
-			background-color={"--dark"}
-			opacity={"0.6"}
-			hover-opacity={"1"}
-			color={"--light"}
-			cursor={"pointer"}
-			transition={"--opacityOut"}
-			quarkly-title={"Badge"}
-			text-decoration-line={"initial"}
-			href={"https://quarkly.io/"}
-			target={"_blank"}
-		>
-			Made on Quarkly
-		</Link>
+	
 		<RawHtml>
 			<style place={"endOfHead"} rawKey={"60d350285179a1001e683fe6"}>
 				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
